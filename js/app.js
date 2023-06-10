185f4671a344dbc0ca969850000d7408ded98b31
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-06-10 16:09:54 +0500

    up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 let toggleBars = document.getElementById("toggle-bar");
-let toggleBar=()=>{
-let nav= document.getElementById("nav-links")
-nav.classList.toggle("active-nav")
-}
+let toggleBar = () => {
+  let nav = document.getElementById("nav-links");
+  nav.classList.toggle("active-nav");
+};
+
+window.addEventListener("scroll", function () {
+  let header = this.document.querySelector(".nav");
+
+  header.classList.toggle("sticky-header", window.scrollY > 150);
+});
+window.addEventListener("scroll", function () {
+  let header = this.document.querySelector(".go-top");
+
+  header.classList.toggle("go-top-active", window.scrollY > 150);
+});
